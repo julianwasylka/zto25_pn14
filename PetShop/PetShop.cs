01da9725fad9f4b788cf7436863bd2738e6f4250
Julian Wasylka <118024381+julianwasylka@users.noreply.github.com>
2025-11-03 15:17:22 +0100

    Classes 03.11.2025

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -40,5 +40,45 @@ namespace Training.DomainClasses
                 result.Sort((p1,p2)=>p1.name.CompareTo(p2.name));
             return result;
         }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            return _petsInTheStore.AllPetsThatSatisfy(pet => pet.species == Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            return _petsInTheStore.AllPetsThatSatisfy(pet => pet.sex == Sex.Female);
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            return _petsInTheStore.AllPetsThatSatisfy(pet => pet.species == Species.Cat || pet.species == Species.Dog);
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            return _petsInTheStore.AllPetsThatSatisfy(pet => pet.species != Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            return _petsInTheStore.AllPetsThatSatisfy(pet => pet.yearOfBirth > 2010);
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            return _petsInTheStore.AllPetsThatSatisfy(pet => pet.yearOfBirth > 2010 && pet.species == Species.Dog);
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            return _petsInTheStore.AllPetsThatSatisfy(pet => pet.sex > Sex.Male && pet.species == Species.Dog);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            return _petsInTheStore.AllPetsThatSatisfy(pet => pet.yearOfBirth > 2010 || pet.species == Species.Rabbit);
+        }
     }
 }
